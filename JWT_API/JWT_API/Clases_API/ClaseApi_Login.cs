583c669a5499fb948aa60158351993802212c38f
zambrano tirado <lzambranot06@gmail.com>
2024-08-16 17:43:30 -0500

    JWT_API ANTES DE INSTALAR PLUGINS

---
+++ JWT_API/JWT_API/Clases_API/ClaseApi_Login.cs
@@ -0,0 +1,11 @@
+﻿
+namespace JWT_API.Clases_API
+{
+    public class ClaseApi_Login
+    {
+        public required String login { get; set; }
+        public required String password { get; set; }
+        public DateOnly tiempo { get; set; }
+
+    }
+}
---
+++ JWT_API/JWT_API/Controllers/Api_LoginController.cs
@@ -0,0 +1,115 @@
+﻿using JWT_API.Clases_API;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Api_loginController : ControllerBase
+    {
+
+        private readonly ILogger<Api_loginController> _logger;
+
+        public Api_loginController(ILogger<Api_loginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "ClaseApi_Login")]
+        public IEnumerable<ClaseApi_Login> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new ClaseApi_Login
+            {
+                login="usuario_login",
+                password="contraseña_login",
+                tiempo = DateOnly.FromDateTime(DateTime.Now.AddDays(index))
+            })
+            .ToArray();
+        }
+    }
+}
+
+/*
+
+    public class Api_LoginController : ControllerBase
+    {
+        private readonly ILogger<Api_LoginController> _logger;
+
+        public Api_LoginController(ILogger<Api_LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetClaseApi_Login")]
+        public IEnumerable<ClaseApi_Login> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new ClaseApi_Login
+            {
+                tiempo = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                login = "soy login",
+                password = "soy password"
+            })
+            .ToArray();
+        }
+*/
+
+        /*
+        // POST: Api_LoginController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        } */
+
+
+        /*
+        // GET: Api_LoginController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Api_LoginController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Api_LoginController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Api_LoginController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        } */
+
